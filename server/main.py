127765a848c9b4e5f993bd409e2fd963356ba18b
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2021-02-12 09:45:25 +1000

    Add licence and document what we need to do to fork slave processes
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- server/main.py
+++ server/main.py
@@ -12,6 +12,12 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(('0.0.0.0', 445))
+        # For now we only run as a single process and we will terminate once
+        # the a client has connected and then disconnected.
+        # We should change this to run the master process to only
+        # listen to the socket, then immediately accept and fork a slave
+        # process to manage the connection.
+        # Having one one process for each connected client.
         s.listen()
         conn, addr = s.accept()
         with conn:
